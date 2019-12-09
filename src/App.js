fca34d2c36306f3dea7f20b9a9164438f0b796a3
Pivobier <pivobier@gmail.com>
2019-12-09 21:11:34 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          H3llo there : Edit <code>src/App.js</code> and save to reload.
+          H3llo there: Edit <code>src/App.js</code> and save to reload. <br>
+          I was changed on feature branch.
         </p>
         <a
           className="App-link"
